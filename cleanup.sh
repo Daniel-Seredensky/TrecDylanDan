7cc529b9e650250c168fcafe6b0e2c279c47eea5
Daniel Seredensky <danielseredensky@gmail.com>
2025-08-19 02:53:42 -0400

    almighty push

--- cleanup.sh
+++ cleanup.sh
@@ -2,12 +2,15 @@ rm -rf ./DerivedData/SearchResults
 rm -rf ./DerivedData/ContextBuilder
 rm -rf ./DerivedData/Evaluation
 rm -rf ./DerivedData/Report
+rm -rf ./DerivedData/Throughput
 mkdir -p ./DerivedData/SearchResults
 mkdir -p ./DerivedData/ContextBuilder
 mkdir -p ./DerivedData/Evaluation
 mkdir -p ./DerivedData/Report
+mkdir -p ./DerivedData/Throughput
 touch ./DerivedData/SearchResults/.gitkeep
 touch ./DerivedData/ContextBuilder/.gitkeep
 touch ./DerivedData/Evaluation/.gitkeep
 touch ./DerivedData/Report/.gitkeep
+touch ./DerivedData/Throughput/.gitkeep
 
